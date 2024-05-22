bcf58a50997dbaf9834afd9b3b26a9e1fe4686cd
Akash Bhowmick <akashbhowmick@akashs-laptop.mynetworksettings.com>
2024-05-22 12:38:31 -0400

    modify api keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
+
+// sections or components
+import { Footer } from "./Components/Footer/Footer";
+// import { Cart } from "./Pages/Cart/Cart";
+import { AuthProvider } from "./providers/AuthProvider";
+import { Routes } from "./Components/Navbar/Router";
 
 function App() {
   return (
     <>
-      <div></div>
+      <AuthProvider>
+        <RouterProvider router={Routes()} />
+        <Footer />
+      </AuthProvider>
     </>
   );
 }
--- src/utils/ApiKeys.ts
+++ src/utils/ApiKeys.ts
@@ -1,4 +1,4 @@
 export const supabasePassword = "aQE2Sr5oPSxwzmDI";
-export const ProjectURL = "https://tjqnstoqmvfjasxllifa.supabase.co";
-export const APIKey =
+export const VITE_SUPABASE_URL = "https://tjqnstoqmvfjasxllifa.supabase.co";
+export const VITE_SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqcW5zdG9xbXZmamFzeGxsaWZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzNDA1MDYsImV4cCI6MjAzMTkxNjUwNn0.nnpvZIbPXHRMse0oVQAfLDxTzBQoeQFUTKiX4qHuTlo";
