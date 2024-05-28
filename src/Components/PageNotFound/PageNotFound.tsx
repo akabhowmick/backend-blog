8cf2db8c4a11efb15d2e6fa50b8bd05b912f3e54
Akash Bhowmick <akabhowmick@gmail.com>
2024-05-27 21:24:02 -0400

    redo footer and add account

--- src/Components/PageNotFound/PageNotFound.tsx
+++ src/Components/PageNotFound/PageNotFound.tsx
@@ -19,7 +19,7 @@ export const PageNotFound = () => {
           <i className="mdi mdi-book-open-page-variant-outline mx-aut text-indigo-800 text-2xl md:text-3xl"></i>
         </div>
         <div className="grow flex flex-col pl-5 pt-2">
-          <div className="font-bold text-sm md:text-lg lg:text-xl group-hover:underline">
+          <div className="font-bold text-black text-sm md:text-lg lg:text-xl group-hover:underline">
             {page.linkHeader}
           </div>
           <div className="font-semibold text-sm md:text-md lg:text-lg text-gray-400 group-hover:text-gray-500 transition-all duration-200 delay-100">
@@ -33,18 +33,15 @@ export const PageNotFound = () => {
   });
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-white py-48">
+    <div className="flex items-center justify-center min-h-screen bg-white py-24">
       <div className="flex flex-col">
         <div className="flex flex-col items-center">
           <div className="text-indigo-500 font-bold text-7xl">404</div>
-          <div className="font-bold text-3xl xl:text-7xl lg:text-6xl md:text-5xl mt-10">
+          <div className="font-bold text-3xl text-black xl:text-7xl lg:text-6xl md:text-5xl">
             This page does not exist
           </div>
-          <div className="text-gray-400 font-medium text-sm md:text-xl lg:text-2xl mt-8">
-            The page you are looking for could not be found.
-          </div>
         </div>
-        <div className="flex flex-col mt-48">
+        <div className="flex flex-col mt-12">
           <div className="text-gray-400 font-bold uppercase">Continue With</div>
           <div className="flex flex-col items-stretch mt-5">{redirectLinks}</div>
         </div>
@@ -52,3 +49,5 @@ export const PageNotFound = () => {
     </div>
   );
 };
+
+// https://tailwindcomponents.com/component/simple-404-page-template
